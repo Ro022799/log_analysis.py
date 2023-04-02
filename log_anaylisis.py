a42c009fbb8ecdc4f5c5700c299576ceec8fe6a0
Your Name <your@example.com>
2023-04-01 19:12:20 -0600

    Add a class Users, contains information about the user Add yeld function, to iterate the user information one by one Eliminate Multiprocessing function

--- log_anaylisis.py
+++ log_anaylisis.py
@@ -3,55 +3,55 @@ import re
 import sys
 import csv
 import operator
+from typing import NamedTuple
 from concurrent.futures import ProcessPoolExecutor
-per_user= {}
-errors = {}
-def dictados():
-	lista = []
+class Users(NamedTuple):
+	action: str
+	message: str
+	user: str
+def informacion():
 	filename = sys.argv[1]
-	with open(filename,mode = 'r', encoding= 'UTF-8') as file:
-		for line in file:
-			lista.append(line)
-		return lista
-def diccionarios(line):        	
-	result = re.search(r'ticky: (INFO|ERROR) (.*)(\[.*\]|\s)+\((\w.*)+\)+$',line)
-	if result == None:
-		Pass
-	action = result.groups()[0]
-	message = result.groups()[1]
-	user = result.groups()[3]
-	if user not in per_user.keys():
-		per_user[user] = {}
-		per_user[user]['INFO'] = 0
-		per_user[user]['ERROR']= 0
-	if action == 'ERROR':
-		errors[message] = errors.get(message, 0) +1
-		per_user[user]['ERROR'] +=1
-	if action == 'INFO':
-		per_user[user]['INFO'] +=1
-	dict_error = sorted(errors.items(), key= operator.itemgetter(1), reverse= True)
-	dict_user = sorted(per_user.items())
-	return dict_error, dict_user
-#if __name__ == '__main__':
-	#with ProcessPoolExecutor(max_workers=1) as executor:
-		#a = list(executor.map(diccionarios, dictados()))
-	#error, user=a[len(a)-1]
-	
+	with open(filename,mode = 'r', encoding ='UTF-8') as file:
+		lines = file.readlines()
+		for line in lines:
+			result = re.search(r'ticky: (INFO|ERROR) (.*)(\[.*\]|\s)+\((\w.*)+\)+$',line)
+			if result == None:
+				pass
+			action = result.groups()[0]
+			message = result.groups()[1]
+			user = result.groups()[3]
+			yield Users(action, message, user)
 
-def csv_generated(file1, file2):
-    with open('error_message.csv', 'w') as files:
-            file_1 = csv.writer(files)
-            file_1.writerow(['ERROR','Count'])
-            for error, mess in file1:
-                file_1.writerow([error,mess])
+
+def diccionarios():
+	per_user ={}
+	errors ={}
+	for token in informacion():
+		if token.user not in per_user.keys():
+			per_user[token.user] = {}
+			per_user[token.user]['INFO'] = 0
+			per_user[token.user]['ERROR']= 0
+		if token.action == 'ERROR':
+			errors[token.message] = errors.get(token.message, 0) +1
+			per_user[token.user]['ERROR'] +=1
+		elif token.action == 'INFO':
+			per_user[token.user]['INFO'] +=1
+	errors= sorted(errors.items(), key= operator.itemgetter(1), reverse= True)
+	per_user = sorted(per_user.items())
+	return per_user, errors
+per_user, errors = diccionarios()
+
+def csv_generated(per_user, errors):
+    with open('error_message.csv', 'w') as file:
+            file_1 = csv.writer(file)
+            file_1.writerow(['ERROR','COUNT'])
+            for error, message in errors:
+                file_1.writerow([error,message])
     with open('user_statistics.csv', 'w') as file:
             file_1=csv.writer(file)
-            file_1.writerow(['Username', 'INFO', 'ERROR'])
-            for i in range(len(file2)):
-                    user, mess = file2[i]
-                    file_1.writerow([user, mess['INFO'], mess['ERROR']])
-if __name__ == '__main__':
-	with ProcessPoolExecutor(max_workers=1) as executor:
-		a = list(executor.map(diccionarios, dictados()))
-	error, user=a[len(a)-1]
-	csv_generated(error,user)
+            file_1.writerow(['USERNAME', 'INFO', 'ERROR'])
+            for pair in per_user:
+                    user, message = pair
+                    file_1.writerow([user, message['INFO'], message['ERROR']])
+
+csv_generated(per_user, errors)
