ae922dcb942b62dda31c8b247ad732cfc45857d1
Your Name <your@example.com>
2023-04-26 09:43:27 -0600

    Add Errors to the functions Write comments to acknowledge what the functions does Create a class User Delete modules ProcessPoolExecutor

--- log_program/log_anaylisis.py
+++ log_program/log_anaylisis.py
@@ -1,16 +1,34 @@
 #!/usr/bin/env python
 import re
+import os
 import sys
 import csv
 import operator
+from tarfile import ExtractError
 from typing import NamedTuple
-from concurrent.futures import ProcessPoolExecutor
 class Users(NamedTuple):
 	action: str
 	message: str
 	user: str
-def informacion():
-	filename = sys.argv[1]
+
+"""Checks the number of paramateres passed into the function"""
+def parameters(number)->int:
+	if len(sys.argv) !=2:
+		raise ValueError('Program requires two parameters instead of {}\n'.format(number))
+	else:
+		return 1
+"""Checks if the given filename have the correct paramaters"""
+def file(filename:str)->str:
+	if type(filename) not in [str]:
+		raise TypeError ('File must be a string\n')
+	if re.match(r'^[a-zA-z\d]+\.[l][o][g]+$', filename) == None:
+		raise TypeError ('File must be a log file')
+	if os.path.exists(filename) != True:
+		raise FileNotFoundError('{} does not exists\n'.format(filename))
+	else:
+		return filename
+"""Iterate over the log file to check the information"""
+def information(filename:str):	
 	with open(filename,mode = 'r', encoding ='UTF-8') as file:
 		lines = file.readlines()
 		for line in lines:
@@ -21,11 +39,11 @@ def informacion():
 			message = result.groups()[1]
 			user = result.groups()[3]
 			yield Users(action, message, user)
-
+"""Creates the dictonaries in a sorted way"""
 def diccionarios():
 	per_user ={}
 	errors ={}
-	for token in informacion():
+	for token in information(filename):
 		if token.user not in per_user.keys():
 			per_user[token.user] = {}
 			per_user[token.user]['INFO'] = 0
@@ -35,11 +53,14 @@ def diccionarios():
 			per_user[token.user]['ERROR'] +=1
 		elif token.action == 'INFO':
 			per_user[token.user]['INFO'] +=1
+	"""If dictionaries does not have information raise ExtractError"""
+	if len(per_user)== 0 and len(errors) ==0:
+		raise ExtractError('Dictionaries without information')
 	errors= sorted(errors.items(), key= operator.itemgetter(1), reverse= True)
 	per_user = sorted(per_user.items())
-	return per_user, errors
-per_user, errors = diccionarios()
 
+	return per_user, errors
+"""Creates the csv files"""
 def csv_generated(per_user, errors):
     with open('error_message.csv', 'w') as file:
             file_1 = csv.writer(file)
@@ -52,5 +73,13 @@ def csv_generated(per_user, errors):
             for pair in per_user:
                     user, message = pair
                     file_1.writerow([user, message['INFO'], message['ERROR']])
+	
 
-csv_generated(per_user, errors)
+if __name__ == '__main__':
+	number = parameters(len(sys.argv))
+	filename = sys.argv[number]
+	filename = file(filename)
+	information(filename)
+	user, error =diccionarios()
+	csv_generated(user, error)
+	sys.exit(0)
